1ba8b66049b7099df550682dfcae0463f794d0f9
VishalKabre <VishalKabre@DESKTOP-HHEF1A7.in.ibm.com>
2019-07-15 13:49:20 +0530

    Updated Chrome driver path with local path.
    Signed-off-by: VishalKabre <VishalKabre@DESKTOP-HHEF1A7.in.ibm.com>

--- final-framework/src/com/training/utility/Driver.java
+++ final-framework/src/com/training/utility/Driver.java
@@ -8,7 +8,7 @@ public interface Driver {
 	String PHANTOM="phantomjs.binary.path";
 	
 	// PATH 
-	String CHROME_PATH="C:\\OnlineTraining\\SDET Oct 2018\\dependencies\\chromedriver.exe";
+	String CHROME_PATH="C:\\Users\\VishalKabre\\Desktop\\Vishalsk\\VishalSK\\Selenium\\chromedriver_win32\\chromedriver.exe";
 	String FIREFOX_PATH ="";
 	String ID="";
 	String PHANTOM_PATH="";
